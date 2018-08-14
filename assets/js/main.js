9588d2e07a7c3c02f1abf6b45d58c97ccfb3d30c
Ali Nazari <ali.nazari@aut.ac.ir>
2018-08-14 10:17:12 +0430

    animated graph set to full screen

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function w3_open_sideBar() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close_sideBar() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+function w3_open_infoBar() {
+    document.getElementById("myInfoBar").style.display = "block";
+}
+function w3_close_infoBar() {
+    document.getElementById("myInfoBar").style.display = "none";
+}
