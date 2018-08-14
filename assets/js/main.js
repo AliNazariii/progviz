a6b24d3e963dcd7442a92d6b805995b5c7ea61d5
Ali Nazari <ali.nazari@aut.ac.ir>
2018-08-14 11:55:37 +0430

    transition added to the sideBars and sideBar buttons improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 function w3_open_sideBar() {
-    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("mySidebar").style.width = "120px";
 }
 function w3_close_sideBar() {
-    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("mySidebar").style.width = "0";
 }
 function w3_open_infoBar() {
-    document.getElementById("myInfoBar").style.display = "block";
+    document.getElementById("myInfoBar").style.width = "300px";
 }
 function w3_close_infoBar() {
-    document.getElementById("myInfoBar").style.display = "none";
+    document.getElementById("myInfoBar").style.width = "0";
 }
