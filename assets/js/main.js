23d23bdce88e95d6a6b5363ade7aa6f0b7fc7c0a
Ali Nazari <ali.nazari@aut.ac.ir>
2018-08-17 23:16:14 +0430

    add ui for change theme in setting modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
-function w3_open_sideBar() {
+function openSideBar() {
     document.getElementById("mySidebar").style.width = "120px";
 }
-function w3_close_sideBar() {
+function closeSideBar() {
     document.getElementById("mySidebar").style.width = "0";
 }
-function w3_open_infoBar() {
+
+function openInfoBar() {
     document.getElementById("myInfoBar").style.width = "300px";
 }
-function w3_close_infoBar() {
+function closeInfoBar() {
     document.getElementById("myInfoBar").style.width = "0";
+}
+
+function closeSetting() {
+    document.getElementById('settingModal').style.display= "none";
+}
+function openSetting() {
+    document.getElementById('settingModal').style.display="block";
+}
+
+function lightTheme() {
+    document.getElementById('lightTheme').classList.add("w3-blue w3-hover-blue");
+    document.getElementById('lightTheme').classList.add("w3-gray w3-hover-blue");
+}
+function darkTheme() {
+    document.getElementById('darkTheme').classList.add("w3-blue w3-hover-blue");
+    document.getElementById('darkTheme').classList.add("w3-gray w3-hover-blue");
+    document.getElementById("darkTheme").style.color = "blue";
 }
