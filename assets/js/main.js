f93e80b5f5b18f9b21b6750efa60fd95dd7e2fe6
Ali Nazari <ali.nazari@aut.ac.ir>
2018-08-19 11:58:08 +0430

    change theme for #body and #leftSideBar and #browse

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,27 @@ function lightTheme() {
     w3.addClass('#lightTheme','w3-blue');
     w3.removeClass('#darkTheme','w3-blue');
     w3.addClass('#darkTheme','w3-light-gray');
+
+    w3.removeClass('#bodyID', 'w3-dark-gray');
+    w3.addClass('#bodyID','w3-light-gray');
+
+    w3.removeClass('#mySidebar', 'w3-teal');
 }
 function darkTheme() {
     w3.removeClass('#darkTheme','w3-light-gray');
     w3.addClass('#darkTheme','w3-blue');
     w3.removeClass('#lightTheme','w3-blue');
     w3.addClass('#lightTheme','w3-light-gray');
+
+    w3.removeClass('#bodyID','w3-light-gray');
+    w3.addClass('#bodyID', 'w3-dark-gray');
+    
+    w3.addClass('#mySidebar', 'w3-teal');
+
+    w3.removeClass('#browse','w3-text-blue');
+    w3.addClass('#browse', 'w3-text-white');
+    w3.removeClass('#browse','w3-hover-blue');
+    w3.addClass('#browse', 'w3-hover-white');
 }
 
+ 
