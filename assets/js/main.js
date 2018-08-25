94c945da727ae727b2bd8a32f146761101731bbd
Ali Nazari <ali.nazari@aut.ac.ir>
2018-08-25 20:32:49 +0430

    change buttons language + change color of language setting

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,7 @@ function lightTheme() {
     w3.addClass('#browse', 'w3-hover-blue');
     w3.removeClass('#browse','w3-hover-text-dark-gray');
     w3.addClass('#browse', 'w3-hover-text-white');
+    document.getElementById('browseText').style.backgroundColor= "#2196f3";
 
     //shot button change theme
     w3.removeClass('#shot', 'w3-text-white');
@@ -49,6 +50,7 @@ function lightTheme() {
     w3.addClass('#shot', 'w3-hover-blue');
     w3.removeClass('#shot','w3-hover-text-dark-gray');
     w3.addClass('#shot', 'w3-hover-text-white');
+    document.getElementById('shotText').style.backgroundColor= "#2196f3";
 
     //zoomIn button change theme
     w3.removeClass('#zoomIn', 'w3-text-white');
@@ -57,6 +59,7 @@ function lightTheme() {
     w3.addClass('#zoomIn', 'w3-hover-blue');
     w3.removeClass('#zoomIn','w3-hover-text-dark-gray');
     w3.addClass('#zoomIn', 'w3-hover-text-white');
+    document.getElementById('zoomInText').style.backgroundColor= "#2196f3";
 
     //zoomOut button change theme
     w3.removeClass('#zoomOut', 'w3-text-white');
@@ -65,7 +68,7 @@ function lightTheme() {
     w3.addClass('#zoomOut', 'w3-hover-blue');
     w3.removeClass('#zoomOut','w3-hover-text-dark-gray');
     w3.addClass('#zoomOut', 'w3-hover-text-white');
-
+    document.getElementById('zoomOutText').style.backgroundColor= "#2196f3";
 
     //setting button change theme
     w3.removeClass('#setting', 'w3-text-white');
@@ -74,6 +77,7 @@ function lightTheme() {
     w3.addClass('#setting', 'w3-hover-blue');
     w3.removeClass('#setting','w3-hover-text-dark-gray');
     w3.addClass('#setting', 'w3-hover-text-white');
+    document.getElementById('settingText').style.backgroundColor= "#2196f3";
 
 
     //setting modal change theme 
@@ -109,7 +113,8 @@ function lightTheme() {
     w3.addClass('#search', 'w3-hover-blue');
     w3.removeClass('#search','w3-hover-text-dark-gray');
     w3.addClass('#search', 'w3-hover-text-white');
-    
+    document.getElementById('searchText').style.backgroundColor= "#2196f3";
+
     //closeLeftSideBar button change theme
     w3.removeClass('#closeLeftSideBar', 'w3-text-white');
     w3.addClass('#closeLeftSideBar','w3-text-blue');
@@ -158,6 +163,7 @@ function darkTheme() {
     w3.addClass('#browse', 'w3-hover-teal');
     w3.removeClass('#browse','w3-hover-text-white');
     w3.addClass('#browse', 'w3-hover-text-dark-gray');
+    document.getElementById('browseText').style.backgroundColor= "#009688";
 
     //shot button change theme
     w3.removeClass('#shot','w3-text-blue');
@@ -166,6 +172,7 @@ function darkTheme() {
     w3.addClass('#shot', 'w3-hover-teal');
     w3.removeClass('#shot','w3-hover-text-white');
     w3.addClass('#shot', 'w3-hover-text-dark-gray');
+    document.getElementById('shotText').style.backgroundColor= "#009688";
     
     //zoomIn button change theme
     w3.removeClass('#zoomIn','w3-text-blue');
@@ -174,6 +181,7 @@ function darkTheme() {
     w3.addClass('#zoomIn', 'w3-hover-teal');
     w3.removeClass('#zoomIn','w3-hover-text-white');
     w3.addClass('#zoomIn', 'w3-hover-text-dark-gray');
+    document.getElementById('zoomInText').style.backgroundColor= "#009688";
 
     //zoomOut button change theme
     w3.removeClass('#zoomOut','w3-text-blue');
@@ -182,6 +190,7 @@ function darkTheme() {
     w3.addClass('#zoomOut', 'w3-hover-teal');
     w3.removeClass('#zoomOut','w3-hover-text-white');
     w3.addClass('#zoomOut', 'w3-hover-text-dark-gray');
+    document.getElementById('zoomOutText').style.backgroundColor= "#009688";
 
     //setting button change theme
     w3.removeClass('#setting','w3-text-blue');
@@ -190,6 +199,7 @@ function darkTheme() {
     w3.addClass('#setting', 'w3-hover-teal');
     w3.removeClass('#setting','w3-hover-text-white');
     w3.addClass('#setting', 'w3-hover-text-dark-gray');
+    document.getElementById('settingText').style.backgroundColor= "#009688";
 
 
     //setting modal change theme 
@@ -225,6 +235,7 @@ function darkTheme() {
     w3.addClass('#search', 'w3-hover-teal');
     w3.removeClass('#search','w3-hover-text-white');
     w3.addClass('#search', 'w3-hover-text-dark-gray');
+    document.getElementById('searchText').style.backgroundColor= "#009688";
 
     //closeLeftSideBar button change theme
     w3.removeClass('#closeLeftSideBar','w3-text-blue');
@@ -254,9 +265,32 @@ function darkTheme() {
 }
 
 function persian() {
+    document.getElementById('browseText').innerText= "انتخاب";
+    document.getElementById('shotText').innerText= "عکس گرفتن";
+    document.getElementById('zoomInText').innerText= "زوم بیشتر";
+    document.getElementById('zoomOutText').innerText= "زوم کمتر";
+    document.getElementById('settingText').innerText= "تنظیمات";
+    document.getElementById('searchText').innerText= "جستجو";    
 
+    //setting modal section change color 
+    w3.removeClass('#englishLanguage','w3-blue');
+    w3.addClass('#englishLanguage','w3-light-gray');
 
+    w3.removeClass('#persianLanguage','w3-light-gray');
+    w3.addClass('#persianLanguage','w3-blue');
 }
 function english() {
+    document.getElementById('browseText').innerText= "Browse";
+    document.getElementById('shotText').innerText= "Shot";
+    document.getElementById('zoomInText').innerText= "ZoomIn";
+    document.getElementById('zoomOutText').innerText= "ZoomOut";
+    document.getElementById('settingText').innerText= "Setting";
+    document.getElementById('searchText').innerText= "Search";    
+    
+    //setting modal section change color 
+    w3.removeClass('#persianLanguage','w3-blue');
+    w3.addClass('#persianLanguage','w3-light-gray');
 
+    w3.removeClass('#englishLanguage','w3-light-gray');
+    w3.addClass('#englishLanguage','w3-blue');
 }
