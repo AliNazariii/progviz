4ac9b139bc2d3347af9e5f31d436a03018c59388
Ali Nazari <ali.nazari@aut.ac.ir>
2018-09-01 08:55:21 +0430

    icon added to the electron browser window

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 900, height: 700})
+    win = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/assets/image/icon.png'});
   
     // and load the index.html of the app.
     win.loadFile('index.html')
